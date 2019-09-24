ea2b08ab467452ba6c9b0472424b32aa801223c2
LittleControl <littlecontrol@qq.com>
2019-09-24 19:22:40 +0800

    清除所有已完成的按钮

--- day04/todomvc-app-template/js/vue/function.js
+++ day04/todomvc-app-template/js/vue/function.js
@@ -3,23 +3,24 @@ const app = new Vue({
     data: {
         seen: false,
         inputText: '',
+        isAllComplete: false,
         todos: [
             {
-                value: 'Sleep', 
+                value: 'Sleep',
                 oriValue: 'Sleep',
                 id: 0,
                 isCompleted: false,
                 isEdit: false
             },
             {
-                value: 'Eat', 
+                value: 'Eat',
                 oriValue: 'Eat',
                 id: 1,
                 isCompleted: false,
                 isEdit: false
             },
             {
-                value: 'Running', 
+                value: 'Running',
                 oriValue: 'Running',
                 id: 2,
                 isCompleted: false,
@@ -30,11 +31,16 @@ const app = new Vue({
     methods: {
         addItems() {
             let obj = {
-                value: this.inputText, 
+                value: this.inputText,
                 oriValue: this.inputText,
-                id: this.todos[this.todos.length - 1].id + 1,
+                // id: this.todos[this.todos.length - 1].id + 1 || 0,
                 isCompleted: false,
                 isEdit: false
+            }
+            if (this.todos.length == 0) {
+                obj.id = 0
+            } else {
+                obj.id = this.todos[this.todos.length - 1].id + 1
             }
             this.todos.push(obj)
             this.inputText = ''
@@ -69,6 +75,22 @@ const app = new Vue({
         },
         test() {
             console.log('LittleControl')
+        },
+        allComplete() {
+            if (this.isAllComplete) {
+                for (const item of this.todos) {
+                    item.isCompleted = false
+                }
+                this.isAllComplete = false
+            } else {
+                for (const item of this.todos) {
+                    item.isCompleted = true
+                }
+                this.isAllComplete = true
+            }
+        },
+        clearCompleted() {
+            this.todos = this.todos.filter(item => item.isCompleted = false)
         }
     }
 })
---
+++ day04/todomvc-app-template/js/vue/test.js
@@ -0,0 +1,39 @@
+let arr = [
+    {
+        id: 1,
+        name: 'Little'
+    },
+    {
+        id: 1,
+        name: 'Control'
+    },
+    {
+        id: 1,
+        name: 'Rain'
+    }
+]
+
+//改变了数组的内容
+/* for (const item of arr) {
+    item.name = 'Little'
+} */
+
+//item为数组下标,可通过下标修改内容
+/* for (const item in arr) {
+    console.log(item)
+    arr[item].name = 'Little'
+} */
+
+// for (const item in arr) {
+//     if (arr[item].id == 1) {
+//         arr.splice(item,1)
+//     }
+// }
+arr = arr.filter(item => {
+    return item.id != 1
+})
+
+console.log(arr);
+
+let obj = {}
+console.log(obj.id);
