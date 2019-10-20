983ab2f12788ed628d761afcddf4986be0a6587a
Avalon <littlecontrol2019@gmail.com>
2019-10-20 20:31:55 +0800

    Vue自定义插件

--- day14/MyPlugin.js
+++ day14/MyPlugin.js
@@ -1,18 +1,19 @@
-(function () {
+(function (window){
     const MyPlugin = {}
     MyPlugin.install = function (Vue, options) {
         Vue.myGlobalMethod = function () {
             console.log('My Global Method')
         }
-        Vue.directive('nothing', {
-            bind (el, binding) {
-                console.log('bind is running')
-            }
+        Vue.directive('nothing', function (el, binding) {
+            console.log(el)
+            console.log(binding)
         })
-        Vue.mixin()
+        //如果这里没有加载组件啥的,一定不要使用mixin方法,不然会导致插件的注册失败
+        //Vue.mixin()
         Vue.prototype.$myMethod = function () {
             console.log('www.littlecontrol.top')
         }
     }
     window.MyPlugin = MyPlugin
-})()
+})(window)
+
