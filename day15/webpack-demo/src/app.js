63d5d95b6249e2c268c957edf911203f500d5f72
Avalon <littlecontrol2019@gmail.com>
2019-10-22 19:53:00 +0800

    webpack 引用JSON

--- day15/webpack-demo/src/app.js
+++ day15/webpack-demo/src/app.js
@@ -1,5 +1,9 @@
 import {f1, f2} from './fun'
+import data from './test.json'
 
 document.write('app is running')
 f1()
 f2()
+console.log(data)
+console.log(typeof data)
+console.log(JSON.stringify(data))
