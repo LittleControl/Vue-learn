51805a4dad7bb1aa1377ce1d5b902dc67742387d
Avalon <littlecontrol2019@gmail.com>
2019-10-22 11:45:54 +0800

    小试牛刀

--- VueDemo/vue_demo/src/App.vue
+++ VueDemo/vue_demo/src/App.vue
@@ -1,13 +1,37 @@
 <template>
-  
+  <div>
+    <p>{{ tip }}</p>
+    <input type="text" v-model="msg">
+    <br />
+    <button @click="add($event)">Add</button>
+    <br />
+    <Commits :dock="dock" />
+  </div>
 </template>
 
 <script>
+import Commits from "./components/Commits";
 export default {
-
+  data() {
+    return {
+      tip: "Please input the words you want",
+      dock: [],
+      msg: ''
+    };
+  },
+  components: {
+      Commits
+  },
+  methods: {
+      add(e) {
+        //   console.log('Nothing')
+          console.log(this.msg)
+          this.dock.push(this.msg)
+          console.log(this.dock)
+      }
+  }
 }
 </script>
 
 <style>
-
 </style>
---
+++ VueDemo/vue_demo/src/components/Commits.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+      <li v-for="item in dock" :key="item">
+          <p>{{ item }}</p>
+      </li>
+  </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        dock: Array
+    },
+}
+</script>
+
+<style>
+
+</style>
