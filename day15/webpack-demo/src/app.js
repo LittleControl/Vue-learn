609c46d2291b58d53cc861143465381ec481c8c2
Avalon <littlecontrol2019@gmail.com>
2019-10-22 20:16:33 +0800

    nothing

--- day15/webpack-demo/src/app.js
+++
@@ -1,9 +0,0 @@
-import {f1, f2} from './fun'
-import data from './test.json'
-
-document.write('app is running')
-f1()
-f2()
-console.log(data)
-console.log(typeof data)
-console.log(JSON.stringify(data))
--- day15/webpack-demo/webpack.config.js
+++ day15/webpack-demo/webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',//入口文件的配置
+    entry: './src/js/index.js',//入口文件的配置
     output: {               //输出文件or出口文件的配置
         filename: 'bundle.js',//输出文件的文件名
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: '/\.css$/',
+                use: ['style-loader', 'css-loader']
+            },
+        ]
     }
 };
