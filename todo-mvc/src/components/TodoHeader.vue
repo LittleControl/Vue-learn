d26fa7e02e46da1942b855630729d049f9c4c707
Avalon <littlecontrol2019@gmail.com>
2019-10-23 17:39:27 +0800

    todoMVC组件化重构

---
+++ todo-mvc/src/components/TodoHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="header">
+    <h1>Vue-todos</h1>
+    <input
+      class="new-todo"
+      placeholder="What needs to be done?"
+      @keyup.enter="addItem($event)"
+      v-focus
+    />
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    addItem(e) {
+      if (e.target.value.trim()) {
+        this.$emit("addItem", e.target.value);
+        e.target.value = "";
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ todo-mvc/src/components/TodoMain.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="main">
+    <input
+      id="toggle-all"
+      class="toggle-all"
+      type="checkbox"
+      @click="toggleAll($event)"
+      :checked="isAllCompleted"
+    />
+    <label for="toggle-all">Mark all as complete</label>
+    <ul class="todo-list">
+      <li
+        v-for="(item, index) in filterTodos"
+        :key="index"
+        :class="{completed: item.isCompleted, editing: currentItem === item}"
+      >
+        <div class="view">
+          <input class="toggle" type="checkbox" v-model="item.isCompleted" />
+          <label @dblclick="toEditing(item)">{{ item.value }}</label>
+          <button class="destroy" @click="deleteItem(index)"></button>
+        </div>
+        <input
+          class="edit"
+          :value="item.value"
+          @keyup.enter="doneEdit(index, $event)"
+          @keyup.esc="cancelEdit(index)"
+          @blur="doneEdit(index, $event)"
+          v-todo-focus="currentItem === item"
+        />
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ["filterTodos", "currentItem", "isAllCompleted"],
+  methods: {
+    deleteItem(index) {
+      this.$emit("deleteItem", index);
+    },
+    toEditing(item) {
+      this.$emit("toEditing", item);
+    },
+    doneEdit(index, e) {
+      let value = e.target.value;
+      this.$emit("doneEdit", { index, value });
+    },
+    cancelEdit(index) {
+      this.$emit("cancelEdit", index);
+    },
+    toggleAll(e) {
+      let res = e.target.checked;
+      this.$emit("toggleAll", res);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
