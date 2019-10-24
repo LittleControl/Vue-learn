158e9eba2aae4fcc625a98665413dda9928348e7
Avalon <littlecontrol2019@gmail.com>
2019-10-24 20:52:43 +0800

    基本路由配置

--- router-demo/src/App.vue
+++ router-demo/src/App.vue
@@ -1,23 +1,34 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <img src="./assets/logo.png" />
+    <div>
+      <router-link to="/">Index</router-link>
+      <router-link to="/header">Header</router-link>
+      <router-link to="/footer">Footer</router-link>
+    </div>
+    <hr />
+    <div>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.router-link-exact-active {
+  color: red;
 }
 </style>
--- router-demo/src/router/index.js
+++ router-demo/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Header from '@/views/Header'
+import Footer from '@/views/Footer'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/header',
+      name: 'Header',
+      component: Header
+    },
+    {
+      path: '/footer',
+      name: 'Footer',
+      component: Footer
     }
   ]
 })
